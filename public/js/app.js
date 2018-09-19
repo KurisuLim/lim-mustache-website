9d2c50de8449a4e78cbf2e0469e846824ff4b90f
KurisuLim <kazuki1002003@yahoo.com>
2018-09-19 06:28:35 -0700

    more changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-/* Your custom app logic goes here */
+var $form = $('form#contact-form'),
+url = 'https://script.google.com/macros/s/AKfycbxfcqTvoqhu03WBQi1nClQNI1XuPpcLXvzli6etOpk1dAhue3M/exec'
+
+$('#submit-form').on('click', function(e) {
+e.preventDefault();
+var jqxhr = $.ajax({
+url: url,
+method: "GET",
+dataType: "json",
+data: $form.serializeObject()
+}).success(
+// do something
+alert('message sent')
+);
+})
